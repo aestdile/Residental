3c4cdad7364e7c5b166a793a9ba4fc030112c82d
aestdile@gmail.com <aestdile@gmail.com>
2025-02-24 23:22:57 +0500

    Code exchanged

--- Nestify.Api/Controllers/HomeController.cs
+++ Nestify.Api/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Residental.Api.Controllers
     {
         [HttpGet]
         public ActionResult<string> Get() =>
-            Ok("Hello Mukhtor, this project is about residental training software");
+            Ok("Hello Mukhtor. Welcome to real project which based on standart requirements!");
     }
 }
 
